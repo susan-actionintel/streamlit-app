5cc412b060e45fe0342b45db505f924f37bd96f3
Susan Olson <129232569+susan-actionintel@users.noreply.github.com>
2023-09-19 19:06:16 -0400

    Barge unloads and locks data added
    Historic USDA locks and unloads with normal comparisons
    All river level graphs shown on tab
    Added manual placeholder for barge unloads predictions

--- tow-volume-analysis.py
+++ tow-volume-analysis.py
@@ -12,9 +12,11 @@ from io import BytesIO
 # S3 info for retrieving static data and writing outputs
 region_name = 'us-east-2'
 bucket_name = 'wait-time-analysis'
+bfpm_bucket_name = 'barge-freight-predictive-modeling'
 static_data_folder = 'static_data/'
 event_data_folder = 'event_testing/'
 ais_event_folder = 'ais-dashboard-events/'
+usda_data_folder = 'usda-barge-data/'
 aws_access_key_id = st.secrets['access_key']
 aws_secret_access_key = st.secrets['secret_key']
 input_filename = 'RawAISDashboardData_current.csv'
@@ -36,8 +38,13 @@ collapsable_note_dict = {'main line grain tows': ('Includes tows over '+str(tow_
                         'all tows': ('Includes all tows over '+str(tow_min_hp_dict['liquids tows'])+'hp')
                         }
 river_location_list = ['St. Louis','Wickliffe','Memphis','Vicksburg','Baton Rouge','New Orleans']
+usda_data_types = ['unloads-nola', 'barge-counts']
+usda_locks_fields = {'lock':['Ark Lock 1','Ohio Olmsted','Miss Locks 27'], #filtering criteria
+                    'type':['Grain, Loaded'],
+                    'direction':['Down']}
 river_stage_bench_years = 10 # past years to consider for river stage benchmarking
 tow_bench_years = 10 # past years to consider for freight rate benchmarking
+usda_bench_years = 10 # past years to consider for usda data benchmarking
 normal_stddev = 1.645 # standard deviations to consider "normal" (90%)
 # %% Define functions
 @st.cache_data(ttl=cache_time_to_live)
@@ -55,7 +62,6 @@ def weekly_benchmark_calc(weekly_data,end_time,years):
     weekly_benchmark_mean = bench_calc.groupby(by=['week_of_year']).mean(numeric_only = True)
     weekly_benchmark_std = bench_calc.groupby(by=['week_of_year']).std(numeric_only = True)
     return weekly_benchmark_mean, weekly_benchmark_std
-# Apply ownership and HP filter
 def apply_owner_hp_filter(data,owner_type,owner_type_dict,minimum_HP):
     if owner_type != 'all tows':
         data = data[(data['owner'].isin(owner_type_dict[owner_type]))]
@@ -73,24 +79,26 @@ with st.spinner('Loading ...'):
     image_data = response['Body'].read()
     logo_image = Image.open(BytesIO(image_data))
     resize_logo = logo_image.resize([200,75])
-    river_tab, eta_tab, unloads_tab, exports_tab = st.tabs(["MS river levels", "Lower MS tow transit",  "Predicted barge unloads", "Predicted NOLA grain exports"])
+    river_tab, eta_tab, unloads_tab, exports_tab = st.tabs(["MS river levels", "Lower MS tow transit",  "Predicted + historic barge unloads", "Predicted NOLA grain exports"])
     with river_tab:
         river_left_col, river_right_col = st.columns([0.3, 0.7],gap = 'medium')
         with river_left_col:
             st.image(resize_logo)
-            st.subheader('User selections')
-            river_location = st.selectbox('Select river location',river_location_list)
-#            st.divider()
+            # st.subheader('User selections')
+            # river_location = st.selectbox('Select river location',river_location_list)
     with eta_tab:
         eta_left_col, eta_right_col = st.columns([0.3, 0.7],gap = 'medium')
         with eta_left_col:
             st.image(resize_logo)
             st.subheader('User selections')
             owner_type = st.selectbox('Select tow type category',['main line grain tows','liquids tows', 'all tows'])
-#            st.divider()
             st.write(collapsable_note_dict[owner_type])
             tow_hp_lower_lim = tow_min_hp_dict[owner_type]
     #       tow_hp_lower_lim = st.slider('Select minimum tow horsepower',value = 6500,min_value = 2000, max_value = 10000, step = 100)
+    with unloads_tab:
+        unloads_left_col, unloads_right_col = st.columns([0.3, 0.7],gap = 'medium')
+        with unloads_left_col:
+            st.image(resize_logo)
     # %% ETL data from daily AIS dashboard monitoring
     tow_data = dataframe_from_s3_csv(bucket_name, ais_event_folder+input_filename)
     tow_data.columns = [col.lower() for col in tow_data.columns]
@@ -114,7 +122,33 @@ with st.spinner('Loading ...'):
     river_data_bench_mean, river_data_bench_std = weekly_benchmark_calc(river_data,pd.to_datetime(end_date),river_stage_bench_years)
     river_data = river_data.merge(river_data_bench_mean,how = 'left',on = 'week_of_year',suffixes = ('','_mean'))
     river_data = river_data.merge(river_data_bench_std,how = 'left',on = 'week_of_year',suffixes = ('','_std'))
-    # %% Create tow count benchmark data
+    # %% ETL USDA data
+    # load usda data from s3
+    usda_data = dict()
+    for data_type in usda_data_types:
+        usda_data[data_type] = dataframe_from_s3_csv(bfpm_bucket_name,usda_data_folder+('usda-data-'+data_type+'.csv'))
+    # usda locks data - formatting and filtering
+    data = pd.DataFrame()
+    data = usda_data['barge-counts'].copy()
+    for col in usda_locks_fields.keys(): #filtering
+        data = data[(data[col].isin(usda_locks_fields[col]))]
+    data['date'] = pd.to_datetime(data['date'])
+    usda_weekly_barge_counts = data[['date','barge_count']].groupby(['date']).sum().dropna()
+    usda_weekly_barge_counts = usda_weekly_barge_counts.sort_values(by=['date']).reset_index()
+    usda_weekly_barge_counts['week_of_year'] = usda_weekly_barge_counts['date'].apply(lambda x: x.weekofyear)
+    usda_counts_mean, usda_counts_std = weekly_benchmark_calc(usda_weekly_barge_counts,pd.to_datetime(end_date),usda_bench_years)
+    usda_weekly_barge_counts = usda_weekly_barge_counts.merge(usda_counts_mean,how = 'left',on = 'week_of_year',suffixes = ('','_mean'))
+    usda_weekly_barge_counts = usda_weekly_barge_counts.merge(usda_counts_std,how = 'left',on = 'week_of_year',suffixes = ('','_std'))
+    # usda barge unloads data - formatting and filtering
+    data = pd.DataFrame()
+    data = usda_data['unloads-nola'].copy()
+    data['date'] = pd.to_datetime(data['date'])
+    usda_weekly_unloads = data[['date','unloads']].dropna()
+    usda_weekly_unloads['week_of_year'] = usda_weekly_unloads['date'].apply(lambda x: x.weekofyear)
+    usda_unloads_mean, usda_unloads_std = weekly_benchmark_calc(usda_weekly_unloads,pd.to_datetime(end_date),usda_bench_years)
+    usda_weekly_unloads = usda_weekly_unloads.merge(usda_unloads_mean,how = 'left',on = 'week_of_year',suffixes = ('','_mean'))
+    usda_weekly_unloads = usda_weekly_unloads.merge(usda_unloads_std,how = 'left',on = 'week_of_year',suffixes = ('','_std')) 
+    # %% ETL tow count benchmark data
     tow_bench_data = pd.DataFrame()
     tow_bench_data = dataframe_from_s3_csv(bucket_name,event_data_folder+tow_benchmark_file)
     tow_bench_data = tow_bench_data[['date','vessel_name','mmsi','location','direction','owner','HP']]
@@ -143,6 +177,15 @@ with st.spinner('Loading ...'):
     tow_benchmark = tow_benchmark.drop(columns=['date']).groupby(by=['week_of_year']).agg(['mean','std'])
     tow_benchmark.columns = ['_'.join(col) for col in tow_benchmark.columns.values] #flattens indexing to one level
     tow_benchmark = tow_benchmark.rename(columns = {'week_of_year_':'week_of_year'})
+    # %% ETL barge unloads prediction results
+    predict_date = usda_weekly_unloads['date'].max()+pd.Timedelta('7D')
+    unloads_predictions = {'Week ending': str(predict_date.date()),
+                           'Predicted value': 519,
+                           'Last week ending': str(usda_weekly_unloads['date'].max().date()),
+                           'Last week predicted': 425,
+                           'Last week reported': 528,
+                           'R2: BargeAI model': 0.82,
+                           'R2: locks linreg': 0.13}
     # %% Daily tow count analysis
     daily_series = pd.date_range(start=start_date, end=end_date, freq='d')
     tows_daily = pd.DataFrame(index=daily_series)
@@ -205,8 +248,7 @@ with st.spinner('Loading ...'):
         title = ('Downstream MS River Tow Traffic (Vicksburg Proxy)')
     )
     fig_d.update_yaxes(
-        zerolinecolor='#4682b4',
-        zerolinewidth = 1,
+        zeroline=False,
         linecolor = 'silver',
         title_text = 'Tows moving toward NOLA <br> (trailing seven days)'  
     )
@@ -248,8 +290,7 @@ with st.spinner('Loading ...'):
         title = ('Upstream MS River Tow Traffic (Vicksburg Proxy)')
     )
     fig_u.update_yaxes(
-        zerolinecolor='#4682b4',
-        zerolinewidth = 1,
+        zeroline=False,
         linecolor = 'silver',
         title_text = 'Tows moving North <br> (trailing seven days)'  
     )
@@ -259,48 +300,91 @@ with st.spinner('Loading ...'):
         linecolor = 'silver',
         range = [start_date, end_date]  
     )
-
-    #river
-    river_data_plot = river_data[(river_data['date']>start_date)]
-    fig_r = go.Figure()
-    river_dates_rev = river_data_plot.loc[::-1,'date']
-    river_norm_upper = river_data_plot[river_location+'_mean']+normal_stddev*river_data_plot[river_location+'_std']
-    river_norm_lower = river_data_plot[river_location+'_mean']-normal_stddev*river_data_plot[river_location+'_std']
-    river_norm_lower = river_norm_lower[::-1] #this is a facet to create the filled line plot
-    fig_r.add_trace(go.Scatter(
-        x = pd.concat([river_data_plot['date'],river_dates_rev]),
-        y = pd.concat([river_norm_upper,river_norm_lower]), 
+    #usda locks data
+    locks_data_plot = usda_weekly_barge_counts[(usda_weekly_barge_counts['date']>start_date)]
+    lock_dates_rev = locks_data_plot.loc[::-1,'date']
+    locks_norm_upper = locks_data_plot['barge_count_mean']+normal_stddev*locks_data_plot['barge_count_std']
+    locks_norm_lower = locks_data_plot['barge_count_mean']-normal_stddev*locks_data_plot['barge_count_std']
+    locks_norm_lower = locks_norm_lower.clip(0)
+    locks_norm_lower = locks_norm_lower[::-1] #this is a facet to create the filled line plot
+    fig_locks = go.Figure()
+    fig_locks.add_trace(go.Scatter(
+        x= pd.concat([locks_data_plot['date'],lock_dates_rev ]),
+        y= pd.concat([locks_norm_upper,locks_norm_lower]), 
         fill='toself',
-        fillcolor='rgba(70, 130, 180,0.4)',
+        fillcolor='rgba(70, 130, 180,0.3)',
         line_color='rgba(255,255,255,0)',
         showlegend=True,
         name='Normal range',
     ))
-    fig_r.add_trace(go.Scatter(
-        x = river_data_plot['date'],
-        y = river_data_plot[river_location], 
-        line_color='sienna',
+    fig_locks.add_trace(go.Scatter(
+        x= locks_data_plot['date'],
+        y= locks_data_plot['barge_count'], 
+        line_color='goldenrod',
         showlegend=True,
-        name='River stage',
+        name='Locking grain barges',
     ))
-    fig_r.update_layout(
+    fig_locks.update_layout(
         plot_bgcolor='black', 
         paper_bgcolor='black',
         xaxis=dict(showgrid=False),
         yaxis=dict(showgrid=False),
         font=dict(color='#c0c0c0'),
-        title = (river_location+' River Stage')
+        title = ('USDA Downstream Locking Grain Barges')
     )
-    fig_r.update_yaxes(
+    fig_locks.update_yaxes(
+        zeroline=False,
+        linecolor = 'silver',
+        title_text = 'Barge count'  
+    )
+    fig_locks.update_xaxes(
         zerolinecolor='#4682b4',
         zerolinewidth = 1,
         linecolor = 'silver',
-        title_text = 'River stage (ft)'  
+        range = [start_date, end_date]  
     )
-    fig_r.update_xaxes(
+    #usda unloads data
+    unloads_data_plot = usda_weekly_unloads[(usda_weekly_unloads['date']>start_date)]
+    unloads_dates_rev = unloads_data_plot.loc[::-1,'date']
+    unloads_norm_upper = unloads_data_plot['unloads_mean']+normal_stddev*unloads_data_plot['unloads_std']
+    unloads_norm_lower = unloads_data_plot['unloads_mean']-normal_stddev*unloads_data_plot['unloads_std']
+    unloads_norm_lower = unloads_norm_lower.clip(0)
+    unloads_norm_lower = unloads_norm_lower[::-1] #this is a facet to create the filled line plot
+    fig_unloads = go.Figure()
+    fig_unloads.add_trace(go.Scatter(
+        x=pd.concat([unloads_data_plot['date'],unloads_dates_rev ]),
+        y= pd.concat([unloads_norm_upper,unloads_norm_lower]), 
+        fill='toself',
+        fillcolor='rgba(70, 130, 180,0.3)',
+        line_color='rgba(255,255,255,0)',
+        showlegend=True,
+        name='Normal range',
+    ))
+    fig_unloads.add_trace(go.Scatter(
+        x=unloads_data_plot['date'],
+        y= unloads_data_plot['unloads'], 
+        line_color='salmon',
+        showlegend=True,
+        name='NOLA barge unloads',
+    ))
+    fig_unloads.update_layout(
+        plot_bgcolor='black', 
+        paper_bgcolor='black',
+        xaxis=dict(showgrid=False),
+        yaxis=dict(showgrid=False),
+        font=dict(color='#c0c0c0'),
+        title = ('USDA NOLA Grain Barge Unloads')
+    )
+    fig_unloads.update_yaxes(
+        zeroline=False,
+        linecolor = 'silver',
+        title_text = 'Barge count'  
+    )
+    fig_unloads.update_xaxes(
         zerolinecolor='#4682b4',
         zerolinewidth = 1,
-        linecolor = 'silver'  
+        linecolor = 'silver',
+        range = [start_date, end_date]  
     )
     # %% Create weekly table for UI display 
     tows_weekly_display = tows_weekly.set_index('date')\
@@ -323,9 +407,56 @@ with river_tab:
     with river_right_col:
         st.title('BargeAI:tm: Lower MS River Intel')
         st.subheader('River Levels')
-        st.plotly_chart(fig_r)
-        st.divider()
+        river_data_plot = river_data[(river_data['date']>start_date)]
+        for river_location in river_location_list:
+            fig_r = go.Figure()
+            river_dates_rev = river_data_plot.loc[::-1,'date']
+            river_norm_upper = river_data_plot[river_location+'_mean']+normal_stddev*river_data_plot[river_location+'_std']
+            river_norm_lower = river_data_plot[river_location+'_mean']-normal_stddev*river_data_plot[river_location+'_std']
+            river_norm_lower = river_norm_lower[::-1] #this is a facet to create the filled line plot
+            fig_r.add_trace(go.Scatter(
+                x = pd.concat([river_data_plot['date'],river_dates_rev]),
+                y = pd.concat([river_norm_upper,river_norm_lower]), 
+                fill='toself',
+                fillcolor='rgba(70, 130, 180,0.4)',
+                line_color='rgba(255,255,255,0)',
+                showlegend=True,
+                name='Normal range',
+            ))
+            fig_r.add_trace(go.Scatter(
+                x = river_data_plot['date'],
+                y = river_data_plot[river_location], 
+                line_color='goldenrod',
+                showlegend=True,
+                name='River stage',
+            ))
+            fig_r.update_layout(
+                plot_bgcolor='black', 
+                paper_bgcolor='black',
+                xaxis=dict(showgrid=False),
+                yaxis=dict(showgrid=False),
+                font=dict(color='#c0c0c0'),
+                title = (river_location+' River Stage')
+            )
+            fig_r.update_yaxes(
+                zeroline=False,
+                linecolor = 'silver',
+                title_text = 'River stage (ft)'  
+            )
+            fig_r.update_xaxes(
+                zerolinecolor='#4682b4',
+                zerolinewidth = 1,
+                linecolor = 'silver'  
+            )
+            st.plotly_chart(fig_r)
 with unloads_tab:
-    st.header('Coming Soon!')
+    with unloads_right_col:
+        st.title('BargeAI:tm: Lower MS River Intel')
+        st.subheader('USDA Barge Unloads Predictions')
+        st.dataframe(pd.DataFrame(unloads_predictions,index = [0]),hide_index=True)
+        st.divider()
+        st.subheader('USDA Locks Data and NOLA Unloads - Downstream Grain Barges')
+        st.plotly_chart(fig_locks)
+        st.plotly_chart(fig_unloads)
 with exports_tab:
     st.header('Coming Soon!')
